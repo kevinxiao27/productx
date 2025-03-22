b1b245843554d5f22b878114e883c3145546d5de
Kevin Tan <kevin2012tan@gmail.com>
2025-03-22 14:10:33 -0700

    package import changes'

---
+++ server/inference/mildKeywords.js
@@ -0,0 +1,45 @@
+export const mildKeywords = [
+  'afraid',
+  'scared',
+  'nervous',
+  'anxious',
+  'worried',
+  'paranoid',
+  'alone',
+  'unsafe',
+  'uncomfortable',
+  'watching me',
+  'being followed',
+  'creepy',
+  'panic',
+  'fear',
+  'hide',
+  'run',
+  'followed',
+  'stalking',
+  'dangerous',
+  'weird',
+  'shady',
+  'threatening',
+  'don’t feel safe',
+  'suspicious',
+  'eerie',
+  'intimidated',
+  'he won’t stop',
+  'please help',
+
+  // 10-Codes that imply suspicious activity
+  '10-30', // Unnecessary use of radio
+  '10-31', // Crime in progress
+  '10-32', // Man with gun (arguably severe)
+  '10-37', // Suspicious vehicle
+  '10-67', // Person calling for help
+  '10-68', // Dispatch info
+
+  '10 30', // Unnecessary use of radio
+  '10 31', // Crime in progress
+  '10 32', // Man with gun (arguably severe)
+  '10 37', // Suspicious vehicle
+  '10 67', // Person calling for help
+  '10 68', // Dispatch info
+];
